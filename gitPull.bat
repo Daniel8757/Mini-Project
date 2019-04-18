54f11ecfeb504d0543f6b49d6579cdb1c68eac84
unknown <sebastian.z.kamal@gmail.com>
2019-04-18 12:57:30 -0400

    Commit

--- gitPull.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\Sebastian\Documents\NetBeansProjects\Game\src\game
-git pull
--- gitPush.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\Users\Sebastian\Documents\NetBeansProjects\Game\src\game
-git add .
-git commit -m "Commit"
-git push
