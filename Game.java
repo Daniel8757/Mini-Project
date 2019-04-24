eb53acba119b7fc17e29759ecfad729977e9b558
unknown <sebastian.z.kamal@gmail.com>
2019-04-24 13:28:58 -0400

    Commit

--- Game.java
+++ Game.java
@@ -4,11 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 
-//gak
-
 /*
-
-hi! :D
 
 Developed by: Kevin Bui, Daniel Li, Sebastian Kamal
 Date of creation: 4/16/19
@@ -18,20 +14,25 @@ Update Log:
 
 4/16/19:
 - Fixed solid borders. 100% working! (Seb)
-- Added optional pacman styled borders (Seb)
+- Added optional pacman styled borders. (Seb)
 
 4/23/19:
 - Individual colours for objects (Seb)
 - Fixed border movement. (Seb)
+- Removed pacman borders. They were uneeded (Seb).
+
+4/24/19:
+- Added jumping! (Non parabolic)(Seb)
 
 */
 
 public class Game extends JPanel implements ActionListener, KeyListener {
-
+    
+    public static boolean canJump = false;
     /* Creates a refresher (the actionlistener in the class ^^ is updating every 5ms) */
     Timer t = new Timer(5, this);
     /* x and y are current points, the vel points are to be added to the x & y to redraw the ball every 5ms which is basically how the movement is done */
-    double x = 360, y = 260, velx = 0, vely = 0;
+    double x = 360, y = 498, velx = 0, vely = 0;
     
     public Game() {
         /* Starts the timer */
@@ -49,92 +50,57 @@ public class Game extends JPanel implements ActionListener, KeyListener {
         g2.setColor(new Color(244,185,66)); // Ball colour
         g2.fill(new Ellipse2D.Double(x, y, 25, 25)); // Ball
         g2.setColor(new Color(0,0,0)); // Status bar colour
-        g2.fill(new Rectangle2D.Double(0, 521, 800, 50)); // Status bar
+        g2.fill(new Rectangle2D.Double(0, 523, 795, 50)); // Status bar
     }
     
     /* Everytime ANYTHING happens (ie. a keypress), it's changing the x & y coords and repainting the canvas with the new ball */
     public void actionPerformed(ActionEvent e) {
         
-        // Solid borders
         // X Borders
-        if ((x + velx >= 0 && x + velx <= 761)) {
+        if ((x + velx >= 0 && x + velx <= 770)) {
             // Allows movement
         } else {
            velx = 0;
         }
         
         // Y Borders
-        if (y + vely >= -1 && y + vely <= 536) {
+        if (y + vely <= 498 && y + vely >= 350) {
             // Allows movement
         } else {
             vely = 0;
         }
         
-        /* Pacman borders (Unlikely to use but cool!)
-        if ((x + velx >= 830)) {
-            x = -35;
-        }
-        if ((x + velx <= -68)) {
-            x = 830;
-        }
-        if ((y + vely >= 605)) {
-            y = -35;
-        }
-        if ((y + vely <= -68)) {
-            y = 600;
-        }
-        */
-        
-        repaint();
         x += velx;
         y += vely;
+        if (y < 498) {
+            y += 10;
+            canJump = false;
+        } else {
+            canJump = true;
+        }
+        repaint();
         
     }
     
-    /* Directional movement handlers */
-    public void up() {
-        vely = -4.5;
-    }
-    
-    public void down() {
-        vely = 4.5;
-    }
-    
-    public void left() {
-        velx = -4.5;
-    }
-    
-    public void right() {
-        velx = 4.5;
-    }
-    
-    /* Key press handling starts here */
+    // Creates controllable movement for the ball
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
         if (code == KeyEvent.VK_UP) {
-            up();
-        }
-        if (code == KeyEvent.VK_DOWN) {
-            down();
+           if (canJump) {
+               vely = -20;
+           }
         }
         if (code == KeyEvent.VK_LEFT) {
-            left();
+            velx = -5.5;
         }
         if (code == KeyEvent.VK_RIGHT) {
-            right();
+            velx = 5.5;
         }
-        // System.out.println("X: " + x + ", Y: " + y);
     }
     
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
-        if (code == KeyEvent.VK_UP) {
-            vely = 0;
-        }
-        if (code == KeyEvent.VK_DOWN) {
-            vely = 0;
-        }
         if (code == KeyEvent.VK_LEFT) {
             velx = 0;
         }
@@ -144,8 +110,8 @@ public class Game extends JPanel implements ActionListener, KeyListener {
     }
     
     @Override
-    public void keyTyped(KeyEvent ke) {
-        System.out.println("Key is not yet implimented in the game!");
+    public void keyTyped(KeyEvent e) {
+        System.out.println("Not yet implemented!");        
     }
     
     public static void main(String [] args) {
@@ -164,5 +130,4 @@ public class Game extends JPanel implements ActionListener, KeyListener {
         f.setSize(800, 600);
     }
      
-    /* End of program */
 }
